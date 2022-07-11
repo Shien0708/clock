202e5f4d1e017020f33959f20871ee769bc94420
Shien <f0928652413@gmail.com>
2022-07-11 12:00:49 +0800

    Initial Commit

---
+++ clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clock
+//
+//  Created by Shien on 2022/7/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
